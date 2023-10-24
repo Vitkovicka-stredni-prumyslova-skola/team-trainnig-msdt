125cc2f3cfb09c7681d070b68909f1aad97251f5
Daniel Dubnický <daniel.dubnicky1@student.vitkovickastredni.cz>
2023-10-24 11:25:43 +0200

    .

--- Corporate/Program.cs
+++ Corporate/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 namespace Corporate{
     class Program{
         public static void Main(string[] args){
+            var e = new Employee("David", 5000);
             
             
 
