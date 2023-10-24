2075f451680a4e5e86895d9f4ddc26a5978d1946
Šimon Šnyta <simon.snyta@student.vitkovickastredni.cz>
2023-10-24 11:05:28 +0200

    .

---
+++ Corporate/Corporate.cs
@@ -0,0 +1,9 @@
+namespace Corporate{
+
+    class Company{
+        public static List<string>employees;
+        public Company(List<string>employess){
+            List<string>employees = new List<string>();
+        }
+    }
+}
