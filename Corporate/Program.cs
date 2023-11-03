87712f5dd236835ec2d53b940f903d6a6fc6d50e
Ondřej Mičaník <micanik.ondrej@student.vitkovikcastredni.cz>
2023-11-03 10:16:01 +0100

    .

--- Corporate/Program.cs
+++ Corporate/Program.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Data.Common;
-using System.Globalization;
-using System.Runtime.CompilerServices;
-using System.Collections.Generic;
-
-namespace Corporate{
-    class Program{
-        public static void Main(string[] args){
-            
-            
-
-
-        }
-    }
-
-}
+﻿
